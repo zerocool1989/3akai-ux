85801d1e4a46b4235253b9fe778171b8eea62879
Christian Vuerings <vueringschristian@gmail.com>
2010-03-02 16:59:48 +0000

    prototype - events - made some changes to the events page

--- dev/_javascript/events.js
+++ dev/_javascript/events.js
@@ -21,14 +21,33 @@
 var sakai = sakai || {};
 sakai.events = function(){
 
+
     /////////////////////////////
     // Configuration variables //
     /////////////////////////////
 
-    var limit = 10;
+    var sendValues = {
+        limit : 10,
+        start_time : [],
+        end_time : [],
+        seconds_before_today : [],
+        seconds_after_today : [],
+        term : []
+    };
 
-    var $eventContainerMain = $("#events_container_list");
+    // CSS Selectors
+    var $eventsContainerList = $("#events_container_list");
+    var $eventsContainerContentSelect = $("#events_container_content select");
     
+    // Form
+    var $eventsFormFilter =$("#events_form_filter");
+    var $eventsShowTalks = $("#events_show_talks", $eventsFormFilter);
+    var $eventsStartDate = $("#events_start_date", $eventsFormFilter);
+    var $eventsEndDate = $("#events_end_date", $eventsFormFilter);
+    var $eventsLimitSecondsBeforeToday = $("#events_limit_seconds_before_today", $eventsFormFilter);
+    var $eventsLimitSecondsAfterToday = $("#events_limit_seconds_after_today", $eventsFormFilter);
+    var $eventsOnlyThisTerm = $("#events_only_this_term", $eventsFormFilter);
+
     // Templates
     var eventsContainerMainTemplate = "events_container_list_template";
 
@@ -42,7 +61,7 @@ sakai.events = function(){
      * @param {Object} results The JSON results object
      */
     var renderTalks = function(results){
-        $eventContainerMain.html($.Template.render(eventsContainerMainTemplate, results));
+        $eventsContainerList.html($.Template.render(eventsContainerMainTemplate, results));
     };
 
 
@@ -70,6 +89,47 @@ sakai.events = function(){
     };
 
     /**
+     * Show shorter date beneath the input field
+     */
+    var showShortDate = function(){
+        var $el = $(this);
+        var date = $el.datepicker("getDate");
+
+        if(date){
+            $("#" + $el.attr("id") + "_formatted").text(parseShortDate(date));
+        }
+        else {
+            $("#" + $el.attr("id") + "_formatted").text("");
+        }
+    };
+
+    /**
+     * Initialise the datepickers
+     */
+    var initDatepicker = function(){
+        var d = new Date();
+        $eventsStartDate.datepicker({
+            buttonText: "Please select a start date",
+            dateFormat: 'dd/mm/y',
+            defaultDate: d,
+            onClose: showShortDate
+        });
+
+        $eventsEndDate.datepicker({
+            buttonText: "Please select an end date",
+            dateFormat: 'dd/mm/y',
+            // We set the default date for the stop date to a week later
+            defaultDate: +7,
+            onClose: showShortDate
+        });
+    };
+
+
+    ////////////////////////
+    // Main functionality //
+    ////////////////////////
+
+    /**
      * Parse the talks into a more usable format for the template
      * @param {Object|Boolean} data 
      * If the data is false it means that there was an error.
@@ -78,21 +138,32 @@ sakai.events = function(){
     var parseTalks = function(data){
         if(data){
             var json = $.evalJSON(data);
-            if (json && json.query && json.query.results && json.query.results.list){
+            if (json && json.vcalendar && json.vcalendar.vevents){
 
-                var talks = json.query.results.list, talk;
+                var talks = json.vcalendar.vevents, talk;
 
-                if(!json.query.results.list.talk){
-                    for (var i=0, il=talks.length; i<il; i++) {
-                        talk = json.query.results.list[i].talk;
-                        json.query.results.list[i].talk.parse_date = parseDate(talk.start_time, talk.end_time);
+                for (var i = 0, il = talks.length; i < il; i++) {
+                    talk = json.vcalendar.vevents[i];
+
+                    // Parse the dates
+                    json.vcalendar.vevents[i].DTPARSE = parseDate($.ParseJCRDate(talk.DTSTART.substring(0, 19)), $.ParseJCRDate(talk.DTEND.substring(0, 19)));
+
+                    // Split the summary since it both contains the name of the speaker + the title of the talk
+                    var splitSummary = json.vcalendar.vevents[i].SUMMARY.split(" - ");
+                    json.vcalendar.vevents[i].SPEAKER = splitSummary[splitSummary.length - 1];
+
+                    json.vcalendar.vevents[i].TITLE = "";
+                    for (var k = 0, kl = splitSummary.length; k < kl -1; k++){
+                        if(k === 0){
+                            json.vcalendar.vevents[i].TITLE += splitSummary[k];
+                        }else{
+                            json.vcalendar.vevents[i].TITLE += " - " + splitSummary[k];
+                        }
+                        
                     }
-                } else{
-                    talk = json.query.results.list.talk;
-                    json.query.results.list.talk.parse_date = parseDate(talk.start_time, talk.end_time);
                 }
 
-                renderTalks(json.query.results);
+                renderTalks(json.vcalendar);
             } else {
                 fluid.log("No featured talks were found");
             }
@@ -106,11 +177,10 @@ sakai.events = function(){
      * Get the featured talks from talks.cam.ac.uk
      */
     var getTalks = function(){
-        var data = {
-            limit: limit
-        };
+
+        // Send of the Ajax request
         $.ajax({
-            data: $.param(data),
+            data: $.param(sendValues),
             url: "/var/proxy/talks/featured_talks.json",
             success: function(data){
                 parseTalks(data);
@@ -121,47 +191,50 @@ sakai.events = function(){
         });
     };
 
-    /**
-     * Bind the change event to the dropdown box where you can
-     * select how many items you want
-     */
-    $(".main_content_container select").live("change", function(){
-        limit = $(".main_content_container select").val();
-        getTalks();
-    });
 
-    /**
-     * Show shorter date beneath the input field
-     */
-    var showShortDate = function(){
-        var $el = $(this);
-        var date = $el.datepicker("getDate");
+    ////////////////////
+    // Binding events //
+    ////////////////////
+
+    var changeSendValues = function(){
+        newSendValues = {
+            limit : $eventsContainerContentSelect.val(),
+            start_time : ($eventsStartDate.datepicker("getDate")) ? $eventsStartDate.datepicker("getDate").getTime()/1000 : [],
+            end_time : ($eventsEndDate.datepicker("getDate")) ? $eventsEndDate.datepicker("getDate").getTime()/1000 : [],
+            seconds_before_today : ($eventsLimitSecondsBeforeToday.val()) ? $eventsLimitSecondsBeforeToday.val() : [],
+            seconds_after_today : ($eventsLimitSecondsAfterToday.val()) ? $eventsLimitSecondsAfterToday.val() : [],
+            term : ($eventsOnlyThisTerm.is(":checked")) ? "current" : []
+        };
         
-        if(date){
-            $("#" + $el.attr("id") + "_formatted").text(parseShortDate(date));
+        if(sendValues !== newSendValues){
+            sendValues = newSendValues;
+            getTalks();
         }
     };
 
     /**
-     * Initialise the datepickers
+     * Add binding to the various elements
      */
-    var initDatepicker = function(){
-        var d = new Date();
-        $("#events_start_date").datepicker({
-            buttonText: "Please select a start date",
-            dateFormat: 'dd/mm/y',
-            defaultDate: d,
-            onClose: showShortDate
+    var addBinding = function(){
+
+        /**
+         * Bind the change event to the dropdown box where you can
+         * select how many items you want
+         */
+        $eventsContainerContentSelect.change(function(){
+            changeSendValues();
         });
 
-        $("#events_end_date").datepicker({
-            buttonText: "Please select an end date",
-            dateFormat: 'dd/mm/y',
-            // We set the default date for the stop date to a week later
-            defaultDate: +7,
-            onClose: showShortDate
+        /**
+         * Add binding to the submit event
+         * This event will be fired if the user hits enter in the form
+         * and if you click on the show feed button
+         */
+        $eventsFormFilter.submit(function(){
+            changeSendValues();
         });
     };
+
 
     ////////////////////
     // Initialisation //
@@ -177,6 +250,9 @@ sakai.events = function(){
         
         // Initialise the datepickers
         initDatepicker();
+        
+        // Add binding to the appropriate elements
+        addBinding();
     };
     
     init();
